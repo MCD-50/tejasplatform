06f56cccd68b811f7c90f639132d96aff98c951b
mcd-50 <ayush.as.shukla@gmail.com>
2020-04-13 12:39:32 +0530

    Fixes

--- app/worker/ticker.js
+++ app/worker/ticker.js
@@ -2,13 +2,17 @@ import moment from "moment";
 
 import * as ticker from "../service/ticker";
 
+import files from "../../files";
+
 // eslint-disable-next-line no-unused-vars
 const initialize = (app) => {
-	// socket
-	setInterval(() => ticker._read(app, "/home/mcd-50/Downloads/tcx.xlsx", moment()), 30);
-
-	// redis
-	setInterval(() => ticker._persist(app, "/home/mcd-50/Downloads/tcx.xlsx", moment()), 1000);
+	files.map(_path => {
+		// socket
+		setInterval(() => ticker._read(app, _path, moment()), 30);
+	
+		// redis
+		setInterval(() => ticker._persist(app, _path, moment()), 1000);
+	})
 };
 
 export default initialize;
